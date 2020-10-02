228deb934ea3a20a8c2e04ac30142380d26f8bf7
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-03 01:35:03 +0300

    Add logger file

---
+++ InfrastructureLayer/ApplicationBase.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfrastructureLayer
+{
+    public class ApplicationBase
+    {
+        protected Logger Log { get; private set; }
+
+        protected ApplicationBase(Type declaringType)
+        {
+            Log = LogManager.GetLogger(declaringType.FullName);
+        }
+    }
+}
---
+++ InfrastructureLayer/EmpVO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfrastructureLayer
+{
+    public class EmpVO : ApplicationBase
+    {
+        public EmpVO() : base(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType)
+        {
+            Log.Info("Instance created ...");
+        }
+    }
+}
