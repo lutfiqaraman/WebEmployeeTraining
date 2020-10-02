564181c1a909dbee72b919de19d9c631e6dd4c4b
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-02 22:14:26 +0300

    Configure Nlog

---
+++ InfrastructureLayer/NLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using NLog.Web;
+
+namespace InfrastructureLayer
+{
+    public class NLogger
+    {
+        public void LoggerConfig()
+        {
+            Logger logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main function");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error in init");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+    }
+}
