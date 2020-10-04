64b082e69c41a991b859a136dda9aa784e7b8c88
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-04 23:38:29 +0300

    Write a dummy test unit

--- UnitTest/InfrastructureLayer/InfrastructureUnitTest.cs
+++ UnitTest/InfrastructureLayer/InfrastructureUnitTest.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace UnitTest.InfrastructureLayer
 {
     [TestClass]
     public class InfrastrctureUnitTest
     {
+
+        public List<string> StringList
+        { get; set; }
+
         [TestMethod]
         public void TestMethod()
         {
+            StringList = new List<string>()
+            {
+                "Test",
+                "Unit"
+            };
 
+            Assert.IsNotNull(StringList);
+            Assert.AreEqual(2, StringList.Count);
         }
     }
 }
