d925866133fcb39d75f9765db25fd3ba5e2a5951
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-06 21:48:00 +0300

    create entities

---
+++ DataAccessLayer/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
